bc8b3763478716aa312061c3f6952439b3ca2fe1
SylviaZhang85 <245993268@qq.com>
2020-03-19 13:25:55 +0800

    贺卡的讲解

--- app/src/main/java/com/swufe/myapplication/MainActivity.java
+++ app/src/main/java/com/swufe/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.swufe.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv= (TextView)findViewById(R.id.showText);
+        tv.setText("swufe");
+
+        EditText inp=(EditText)findViewById(R.id.inpext);
+        String str = inp.getText().toString();
+
+        Log.i("main","input="+str);
     }
 }
