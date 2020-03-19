811c37f9ffd0cfa66ffda12151ec95dca74b06cd
SylviaZhang85 <245993268@qq.com>
2020-03-19 19:10:30 +0800

    事件绑定

--- app/src/main/java/com/swufe/myapplication/MainActivity.java
+++ app/src/main/java/com/swufe/myapplication/MainActivity.java
@@ -4,21 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView out;
+    EditText inp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv= (TextView)findViewById(R.id.showText);
-        tv.setText("swufe");
+        out= (TextView)findViewById(R.id.showText);
+        inp=(EditText)findViewById(R.id.inpext);
 
-        EditText inp=(EditText)findViewById(R.id.inpext);
+        Button btn= (Button)findViewById(R.id.btn1);
+        //btn.setOnClickListener(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("mail","onClick call......");
+                String str = inp.getText().toString();
+
+                out.setText("Hello "+str);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+      Log.i("click","onClick");
+
+        //TextView tv= (TextView)findViewById(R.id.showText);
+
+
+        //EditText inp=(EditText)findViewById(R.id.inpext);
         String str = inp.getText().toString();
 
-        Log.i("main","input="+str);
+        out.setText("Hello "+str);
+    }
+    public void btnClick(View btn){
+        Log.i("click","btnClick called......");
     }
 }
