5decebf4de90b415095e39764dd545ca2cd26ea5
SylviaZhang85 <245993268@qq.com>
2020-04-17 10:09:54 +0800

    Activity生命周期及数据保存(正确版)

--- app/src/main/java/com/swufe/myapplication/SecondActivity.java
+++ app/src/main/java/com/swufe/myapplication/SecondActivity.java
@@ -10,35 +10,64 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 private final String TAG="second";
- TextView score;
- TextView scorea;
+    TextView score;
+    TextView scorea;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-Log.i(TAG,"onCreate:");
-
-
+        Log.i(TAG,"onCreate:");
+        score = (TextView)findViewById(R.id.score);
+        scorea = (TextView)findViewById(R.id.scorea);
     }
+
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
-        String scorea=((TextView)findViewById(R.id.score)).getText().toString();
-        String scoreb=((TextView)findViewById(R.id.scorea)).getText().toString();;
-        outState.putString("teama_score",scorea);
-        outState.putString("teamb_score",scoreb);
+        String score1=((TextView)findViewById(R.id.score)).getText().toString();
+        String score2=((TextView)findViewById(R.id.scorea)).getText().toString();;
+        outState.putString("teama_score",score1);
+        outState.putString("teamb_score",score2);
     }
-
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        String scorea= savedInstanceState.getString("teama_score");
-        String scoreb= savedInstanceState.getString("teamb_score");
+        String score1= savedInstanceState.getString("teama_score");
+        String score2= savedInstanceState.getString("teamb_score");
         Log.i(TAG,"onRestoreInstanceState");
-        ((TextView)findViewById(R.id.score)).setText(scorea);
-        ((TextView)findViewById(R.id.scorea)).setText(scoreb);
+        ((TextView)findViewById(R.id.score)).setText(score1);
+        ((TextView)findViewById(R.id.scorea)).setText(score2);
 
     }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i(TAG,"onStart:");
+    }
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG,"onResume:");
+    }
+    protected void onRestart(){
+        super.onRestart();
+        Log.i(TAG,"onRestart:");
+    }
+    protected void onPause(){
+        super.onPause();
+        Log.i(TAG,"onPause:");
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG,"onDestroy:");
+    }
+    protected void onStop(){
+        super.onStop();
+        Log.i(TAG,"onStop:");
+    }
+
+
+
 
     public void btnAdd1(View btn){
         if(btn.getId()==R.id.btn_1){
